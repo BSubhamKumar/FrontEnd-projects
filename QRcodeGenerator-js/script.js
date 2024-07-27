4de90f5f066480810ba560c9a63e54edad6d643b
B Subham Kumar <bsubhamkumar69@gmail.com>
2024-07-27 19:20:04 +0530

    Add files via upload

---
+++ QRcodeGenerator-js/script.js
@@ -0,0 +1,11 @@
+// const apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR(){
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+}
+// generateQR();
+
